07c34b842f5c681249144c4c57afd4c99af12c8d
Akanksha1197 <167187980+Akanksha1197@users.noreply.github.com>
2024-05-15 14:08:15 +0530

    Update SignIn.tsx

--- frontend/src/components/SignIn/SignIn.tsx
+++ frontend/src/components/SignIn/SignIn.tsx
@@ -16,7 +16,7 @@ const HandleChange=(e:ChangeEvent<HTMLInputElement>)=>{
     e.preventDefault();
     const {email,password}=fromData;
     try {
-      const response = await fetch('http://localhost:5000/api/login',
+      const response = await fetch('https://vercel.com/akankshas-projects-76b3734f/blog-app/api/login',
       {
         method: 'POST',
         headers: {
